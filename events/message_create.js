ae85fd694d30c71aff0ce216bdfd5137ba8daab6
Tyler Westland <tylerofthewest@gmail.com>
2023-11-06 18:39:04 -0600

    Complete minimum angry emoji for pretzel mentioning.

--- events/message_create.js
+++ events/message_create.js
@@ -4,6 +4,12 @@ module.exports = {
 	name: Events.MessageCreate,
 	once: false,
 	execute(message) {
-		console.log(`Message from user ${message.author}: ${message}`);
+        if (message.content.toLowerCase().includes("pretzel")) {
+            console.log("User ${message.author} said pretzel")
+            // https://old.discordjs.dev/#/docs/discord.js/14.13.0/class/Message?scrollTo=react
+            message.react('😠')
+                .then(console.log)
+                .catch(console.error);
+        }
 	},
 };
